2f0cd83a209a1017613ea330e8d323e2f52e5c15
meet <you@example.com>
2018-08-08 14:10:16 -0400

    hi

--- exercises/app.py
+++ exercises/app.py
@@ -2,8 +2,14 @@ from databases import *
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+#@app.route('/home.html')
+#def home():
+ #   return render_template(
+  #  	'home.html')
+@app.route('/<int:student_id>')
+def display_student(student_id):
+	return render_template('student.html',s=student_id)     
 @app.route('/')
-def home():
-    return 'START HERE: CHANGE ME'
-
+def home_route():
+	return render_template('home.html')
 app.run(debug=True)
